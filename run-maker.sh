8a80dea78a15e9bb4d2e0759e64beddef6a3fb3d
lyune <lyune@myhost.mydomain>
2018-10-31 18:38:35 +0900

    add shell

---
+++ run-maker.sh
@@ -0,0 +1,17 @@
+JAVA_OPT="-Djava.net.preferIPv4Stack=true \
+         -Xms1G -Xmx1G \
+         -server \
+         -XX:+UseG1GC \
+         -XX:MaxGCPauseMillis=20 \
+         -XX:InitiatingHeapOccupancyPercent=35 \
+         -XX:+ExplicitGCInvokesConcurrent \
+         -Xloggc:./logs/gc.log \
+         -verbose:gc \
+         -XX:+PrintGCDetails \
+         -XX:+PrintGCDateStamps \ 
+         -XX:+PrintGCTimeStamps \
+         -XX:+UseGCLogFileRotation \
+         -XX:NumberOfGCLogFiles=10 \
+         -XX:GCLogFileSize=100M "
+
+java $JAVA_OPT -jar /arch/maker/msgmaker-0.0.1-SNAPSHOT.jar -cf /arch/consumer/msgmaker.conf $*
