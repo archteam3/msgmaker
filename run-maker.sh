efb2725147bc5755fdb56aadb8f728ef19c9a688
Lyune <Lyune@DESKTOP-20PIKGU>
2018-10-31 23:25:48 +0900

    .

--- run-maker.sh
+++ run-maker.sh
@@ -1,17 +1,3 @@
-JAVA_OPT="-Djava.net.preferIPv4Stack=true \
-         -Xms1G -Xmx1G \
-         -server \
-         -XX:+UseG1GC \
-         -XX:MaxGCPauseMillis=20 \
-         -XX:InitiatingHeapOccupancyPercent=35 \
-         -XX:+ExplicitGCInvokesConcurrent \
-         -Xloggc:./logs/gc.log \
-         -verbose:gc \
-         -XX:+PrintGCDetails \
-         -XX:+PrintGCDateStamps \ 
-         -XX:+PrintGCTimeStamps \
-         -XX:+UseGCLogFileRotation \
-         -XX:NumberOfGCLogFiles=10 \
-         -XX:GCLogFileSize=100M "
+JAVA_OPT="-Djava.net.preferIPv4Stack=true -Xms1G -Xmx1G -server -XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:InitiatingHeapOccupancyPercent=35 -XX:+ExplicitGCInvokesConcurrent -Xloggc:/arch/logs/gc.log -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=10  -XX:GCLogFileSize=100M "
 
-java $JAVA_OPT -jar /arch/maker/msgmaker-0.0.1-SNAPSHOT.jar -cf /arch/consumer/msgmaker.conf $*
+java $JAVA_OPT -jar ./msgmaker-0.0.1-SNAPSHOT.jar $*
